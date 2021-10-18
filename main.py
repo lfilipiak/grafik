a555c44d57b4856e6184c1a72394a2527d61f0de
hapfff <lukaszfilipiak1@gmail.com>
2021-10-19 00:08:54 +0200

    dodano gitignore

--- main.py
+++ main.py
@@ -5,10 +5,34 @@ uwzględnieniem dni wolnych, wolnych dni po 24h dyżuru oraz tak żeby obsadzone
 wystarczającej ilości osób.
 """
 
-import datetime
+import datetime as dt
+import os
 
-def month_to_csv():
-    print(datetime.datetime.month)
-    pass
+import openpyxl
+import openpyxl as opxl
+import pandas as pd
 
-month_to_csv()
+
+def month_to_csv(month):
+    """
+    :param month: pobiera wartość miesiąca od 1 do 12
+    :return: zwraca plik z tabelą z podanym miesiącem
+    """
+    file_name = 'grafik.xls'
+    df = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]], index=['jeden', 'dwa', 'trzy'],
+                      columns=['a', 'b', 'c'])
+    path = r'/home/filip/Pulpit/PythonZ/Projekt - grafik/'
+    wb = openpyxl.load_workbook(file_name)
+    sheet = wb.get_sheet_by_name("spam")
+    sheet.title = "df data"
+    wb.save('save.xls')
+
+    xlr = pd.ExcelWriter('save.xls')
+    df.
+
+        # file.write(str(dt.datetime(year=2021, month=month, day=12)))
+
+    return
+
+
+month_to_csv(1)
