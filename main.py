dd7801df1f3cf6dd5ce56dbdfbd4da3d848db5f1
hapfff <lukaszfilipiak1@gmail.com>
2021-10-18 23:15:57 +0200

    zainicjonowanie gita w projekcie

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Projekt ma za zadanie pobranie plików wsadowych (czyt. godziny oraz prośby) zaczytanie ich, a następnie
+zwrócenie danych w postaci tabeli miesięcznej na zadany rok oraz miesiąc z informacją kto i kiedy ma dyżur, łącznie z
+uwzględnieniem dni wolnych, wolnych dni po 24h dyżuru oraz tak żeby obsadzone były wszystkie dni i godziny w
+wystarczającej ilości osób.
+"""
+
+import datetime
+
+def month_to_csv():
+    print(datetime.datetime.month)
+    pass
+
+month_to_csv()
