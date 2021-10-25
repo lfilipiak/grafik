148eb712c10be4c2c5287d09b20f0ad0dc7e97e7
hapfff <lukaszfilipiak1@gmail.com>
2021-10-25 20:55:20 +0200

    dziala zapisywanie do xls

--- main.py
+++ main.py
@@ -4,35 +4,41 @@ zwrócenie danych w postaci tabeli miesięcznej na zadany rok oraz miesiąc z in
 uwzględnieniem dni wolnych, wolnych dni po 24h dyżuru oraz tak żeby obsadzone były wszystkie dni i godziny w
 wystarczającej ilości osób.
 """
+import calendar
+import datetime
+from openpyxl import Workbook
+from openpyxl.styles import Font
 
-import datetime as dt
-import os
+file_name = 'grafik.xls'
+path = r'/home/filip/Pulpit/PythonZ/Projekt - grafik/'
 
-import openpyxl
-import openpyxl as opxl
-import pandas as pd
-
-
-def month_to_csv(month):
+def month_to_csv(year=2021, month=1):
     """
     :param month: pobiera wartość miesiąca od 1 do 12
     :return: zwraca plik z tabelą z podanym miesiącem
     """
-    file_name = 'grafik.xls'
-    df = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]], index=['jeden', 'dwa', 'trzy'],
-                      columns=['a', 'b', 'c'])
-    path = r'/home/filip/Pulpit/PythonZ/Projekt - grafik/'
-    wb = openpyxl.load_workbook(file_name)
-    sheet = wb.get_sheet_by_name("spam")
-    sheet.title = "df data"
-    wb.save('save.xls')
-
-    xlr = pd.ExcelWriter('save.xls')
-    df.
-
-        # file.write(str(dt.datetime(year=2021, month=month, day=12)))
-
+    wb = Workbook()
+    ws = wb.active
+    xls_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE']
+    list = []
+    cal1 = calendar.Calendar()
+    for day in cal1.itermonthdays2(year, month):
+        if day[0] == 0:
+            continue
+        list.append(day)
+    length_of_the_month = len(list)
+    i = 0
+    while i != length_of_the_month:
+        ws['{}1'.format(xls_list[i])] = str(datetime.date(year, month, list[i][0]))
+        if list[i][1] == 5 or list[i][1] == 6:
+            ws['{}1'.format(xls_list[i])].font = Font(color="FF0000", bold=True)
+        i += 1
+    ws['A5'] = 145
+    wb.save(file_name)
     return
 
 
-month_to_csv(1)
+year = int(input("Podaj rok: "))
+month = int(input("Podaj miesiąc: "))
+month_to_csv(year, month)
